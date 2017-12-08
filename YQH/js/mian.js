da89ac8bae05138aa1479f54ef3748e106c9a86b
xaiogede <2469136989@qq.com>
2017-12-08 18:37:23 +0800

    邀请函初稿

---
+++ YQH/js/mian.js
@@ -0,0 +1,14 @@
+/**
+ * Created by lenovo on 17/12/6 006.
+ */
+//<!-- Initialize Swiper -->
+var swiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'fraction',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
